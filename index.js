b38f51c8aeb6eeadaf4b9d086e75b061bb4e88c1
Raj <raj2356.be21@chitkara.edu.in>
2023-03-06 16:26:30 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const https = require("https");  // require https
+const bodyParse = require("body-parser");
+const app = express();
+
+app.use(bodyParse.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  var cityName = req.body.cityName;
+  var URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=3dec6948d8db1567671c8de1af0ef9a4`;
+  https.get(URL, (response) => {   // to get from external server
+    response.on("data", (data) => {    //both data have same spelling.
+      var fullData = JSON.parse(data);
+      var latitudes = fullData[0].lat;
+      var longs = fullData[0].lon;
+      res.write(`<h1>The Latitude of London ${latitudes}</h1>`);
+      res.write(`<h1>The Longititude of London ${longs}</h1>`);
+      res.send();
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
